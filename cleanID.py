9213d885b70dfd9a94ad5f134513fd9f21b91dd0
Tom Von Lahndorff <tomvonl@gmail.com>
2010-07-31 17:18:02 -0400

    Added cleanid.py script. Useful for sanitizing file ids.

---
+++ cleanID.py
@@ -0,0 +1,34 @@
+#This script will remove "nasty" characters from a string. Useful for sanitizing file ids.
+
+def clean(anyid):
+	
+	import string
+
+	anyid = str(anyid)
+	
+	anyid = string.replace(anyid,' ','_')
+	anyid = string.replace(anyid,'-','_')
+	anyid = string.replace(anyid,'"','')
+	anyid = string.replace(anyid,'\'','')
+	anyid = string.replace(anyid,'&','and')
+	anyid = string.replace(anyid,'\,','')
+	anyid = string.replace(anyid,'!','')
+	anyid = string.replace(anyid,'@','')
+	anyid = string.replace(anyid,'#','')
+	anyid = string.replace(anyid,'$','')
+	anyid = string.replace(anyid,'%','')
+	anyid = string.replace(anyid,'^','')
+	anyid = string.replace(anyid,'*','')
+	anyid = string.replace(anyid,'\(','')
+	anyid = string.replace(anyid,'\)','')
+	anyid = string.replace(anyid,';','')
+	anyid = string.replace(anyid,'|','')
+	anyid = string.replace(anyid,'+','')
+	anyid = string.replace(anyid,'~','')
+	anyid = string.replace(anyid,'\[','')
+	anyid = string.replace(anyid,'\]','')
+	anyid = string.replace(anyid,'\{','')
+	anyid = string.replace(anyid,'\}','')
+	anyid = string.replace(anyid,'?','')
+	
+	return anyid
